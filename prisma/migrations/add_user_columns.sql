90bde0c0b43e4d4a6a6ccb0c648109219a353125
ptre <barrettburnworth@gmail.com>
2025-11-08 11:27:56 -0600

    chore: clean up old migrations, update 0001_init with email-first schema

--- prisma/migrations/add_user_columns.sql
+++
@@ -1,9 +0,0 @@
--- Add missing email, name, and avatar columns to User table
-ALTER TABLE "User" ADD COLUMN "email" TEXT;
-ALTER TABLE "User" ADD COLUMN "name" TEXT;
-ALTER TABLE "User" ADD COLUMN "avatar" TEXT;
-
--- Add indexes
-CREATE UNIQUE INDEX IF NOT EXISTS "User_email_key" ON "User"("email");
-CREATE INDEX IF NOT EXISTS "User_email_idx" ON "User"("email");
-CREATE INDEX IF NOT EXISTS "User_username_idx" ON "User"("username");
